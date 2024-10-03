1f147c5041538c31935e9e51d80ce62f5cc0f4a9
anuri-el <sofia.poteychuk@gmail.com>
2024-10-04 00:01:57 +0300

    faq

--- books/views.py
+++ books/views.py
@@ -5,4 +5,5 @@ from .models import Book
 # Create your views here.
 def books_list(request):
     books = Book.objects.all()
+    # books = Book.objects.all().order_by()
     return render(request, "books/books_list.html", {'books' : books})
